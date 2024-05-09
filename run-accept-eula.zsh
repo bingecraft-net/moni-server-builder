d32f47c63fbd7de7d25631a9828e3c771e9c61d5
tyler <tyler@cruftbusters.com>
2024-05-09 11:56:31 -0700

    nogui

--- run-accept-eula.zsh
+++ run-accept-eula.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-cd server && ./run.sh ; cd -
+cd server && ./run.sh nogui ; cd -
 if grep "You need to agree to the EULA" server/logs/latest.log ; then
   sed -i s/eula=false/eula=true/ server/eula.txt
-  cd server && ./run.sh
+  cd server && ./run.sh nogui
 fi
