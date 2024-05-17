d090d24d1ce39f07e344a543174c36a43c93bd7e
tyler <tyler@cruftbusters.com>
2024-05-17 08:45:18 -0700

    install discord integration

--- install.zsh
+++ install.zsh
@@ -39,11 +39,46 @@ FORGE_TEMPLATE=forge-template-$FORGE_VERSION
 
 }
 
+OVERRIDES_DIR=overrides
+OVERRIDES_MODS_DIR=overrides/mods
+mkdir -p $OVERRIDES_MODS_DIR
+
+DI_VERSION=3.0.7.1-1.20.1
+DI_JAR=dcintegration-forge-$DI_VERSION.jar
+[[ ! -f $OVERRIDES_MODS_DIR/$DI_JAR ]] && {
+
+  curl -sL \
+    -o $OVERRIDES_MODS_DIR/$DI_JAR \
+    https://cdn.modrinth.com/data/rbJ7eS5V/versions/ILJrSvYW/$DI_JAR
+
+}
+
 TARGET_DIR=server
 [[   -d $TARGET_DIR ]] && echo target dir $TARGET_DIR already exists
 [[ ! -d $TARGET_DIR ]] && {
+
   cp -r $FORGE_TEMPLATE $TARGET_DIR
   cp -r $MONI_TEMPLATE/overrides/* $TARGET_DIR
+  cp -r $OVERRIDES_DIR/* $TARGET_DIR
+
+}
+
+{
+
+  declare -a sedArgs
+
+  while read entry ; do
+    key=${entry%%=*}
+    value=${entry#*=}
+    sedArgs+=("-e")
+    sedArgs+=("s|$key|$value|Ig")
+  done < secrets.properties
+
+  find server/config -type f | while read file ; do
+    sed -i.orig "${sedArgs[@]}" $file
+    cmp "$file" "$file.orig" && mv "$file.orig" "$file"
+  done
+
 }
 
 if [[ ! -z $TARGET ]] ; then
