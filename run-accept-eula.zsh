e6f63e12ab074bf431012fe5dd7b75bbc6178623
tyler <tyler@cruftbusters.com>
2024-06-14 13:47:22 -0700

    run-accept-eula reboots server automatically

--- run-accept-eula.zsh
+++ run-accept-eula.zsh
@@ -1,6 +1,13 @@
 #!/bin/zsh
-cd server && ./run.sh nogui ; cd -
-if grep "You need to agree to the EULA" server/logs/latest.log ; then
-  sed -i s/eula=false/eula=true/ server/eula.txt
-  cd server && ./run.sh nogui
+#
+cd server
+
+./run.sh nogui
+
+if grep "You need to agree to the EULA" logs/latest.log ; then
+  sed -i s/eula=false/eula=true/ eula.txt
 fi
+
+while true ; do
+  ./run.sh nogui
+done
