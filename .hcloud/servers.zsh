5ca5bbd70a91ebdc7ec214f5e8fc4f78ac75d5ef
tyler <tyler@cruftbusters.com>
2024-10-02 23:08:54 -0700

    move to nebula-blue

--- .hcloud/servers.zsh
+++ .hcloud/servers.zsh
@@ -1,1 +1,1 @@
-hcloud server create --datacenter ash-dc1 --image ubuntu-24.04 --name nebula-pink --ssh-key arctair@clover --type ccx13
+hcloud server create --datacenter ash-dc1 --image ubuntu-24.04 --name nebula-blue --ssh-key arctair@clover --type ccx13
