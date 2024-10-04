b901ff84829ed42c134973e50417bf8a2849e8a3
tyler <tyler@cruftbusters.com>
2024-10-04 08:53:59 -0700

    add ssh-keys for desktop

--- .hcloud/ssh-keys.zsh
+++ .hcloud/ssh-keys.zsh
@@ -1,2 +1,4 @@
 #!/usr/bin/env zsh
 hcloud ssh-key create --name arctair@clover --public-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEAA3Eu3apXcocTm57io5xEOxliRlOR33sBHZoiPHZw2 arctair@clover"
+hcloud ssh-key create --name arctair@fragrance --public-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0+7zuoZtp1CevNJK6XEvEEFNR8frvn4eqVudUE6qLA arctair@fragrance"
+hcloud ssh-key create --name arctair@DESKTOP-KHEI15E --public-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKs9vHgJ21j6TD4799qkJFIbFhg2jN3UFm5t4HlLBqr3 arctair@DESKTOP-KHEI15E"
