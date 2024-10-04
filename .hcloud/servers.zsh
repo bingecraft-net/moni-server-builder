6d4837179c00d76e26bc067cbbaaf24924442054
tyler <tyler@cruftbusters.com>
2024-10-04 09:48:11 -0700

    add pink server with more resources

--- .hcloud/servers.zsh
+++ .hcloud/servers.zsh
@@ -1,1 +1,2 @@
 hcloud server create --datacenter ash-dc1 --image ubuntu-24.04 --name nebula-blue --ssh-key arctair@clover --type ccx13
+hcloud server create --datacenter ash-dc1 --image ubuntu-24.04 --name nebula-pink --ssh-key arctair@clover --ssh-key arctair@fragrance --ssh-key arctair@DESKTOP-KHEI15E --type ccx23
